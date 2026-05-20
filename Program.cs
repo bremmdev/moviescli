5dbde7061fdadcb03eee256be08a6466c39d273b
bremmdev <bremmdev@gmail.com>
2026-05-20 19:38:26 +0200

    basic command structure

--- Program.cs
+++ Program.cs
@@ -1,8 +1,81 @@
 #:package Microsoft.Data.Sqlite@10.0.8
+#:package Spectre.Console@0.55.2
 
 using Microsoft.Data.Sqlite;
+using Spectre.Console;
 
 using var database = new Database("Data Source=movies.db");
+
+while (true)
+{
+    AnsiConsole.Markup("[grey]> [/]");
+    var input = Console.ReadLine();
+
+    if (input == null || !input.StartsWith('/'))
+    {
+        AnsiConsole.MarkupLine("[red]Invalid command. Commands must start with '/'[/]");
+        continue;
+    }
+
+    CommandHandler handler = new CommandHandler(database);
+    handler.HandleCommand(input.Trim());
+}
+
+sealed class CommandHandler
+{
+    private readonly Database _database;
+    // Dictionary of commands and their corresponding actions, case-insensitive
+    private readonly Dictionary<string, Action> _commands;
+
+    public CommandHandler(Database database)
+    {
+        _database = database;
+        _commands = new(StringComparer.OrdinalIgnoreCase)
+        {
+            ["/help"] = HelpCommand,
+            ["/list"] = ListCommand,
+            ["/exit"] = TerminateSession,
+            ["/quit"] = TerminateSession,
+            ["/q"] = TerminateSession,
+        };
+    }
+
+    public void HandleCommand(string command)
+    {
+        if (!_commands.TryGetValue(command, out Action? action))
+        {
+            AnsiConsole.MarkupLine("[red]Unknown command[/]");
+            return;
+        }
+        action();
+    }
+
+    private static void HelpCommand()
+    {
+        AnsiConsole.MarkupLine("""
+            [bold green]Available commands:[/]
+            [bold blue]/help[/] - Show this help message
+            [bold blue]/list[/] - List all movies
+            [bold blue]/exit[/] - Exit the application
+        """);
+    }
+
+    private void ListCommand()
+    {
+        var movies = _database.GetMovies();
+        AnsiConsole.MarkupLine("List");
+        foreach (var movie in movies)
+        {
+            AnsiConsole.MarkupLine($"[bold green]{movie.Title}[/] ({movie.Year}) - {movie.Genre} - {movie.Rating}");
+        }
+    }
+
+    private static void TerminateSession()
+    {
+        Environment.Exit(0);
+    }
+}
+
 
 sealed class Database : IDisposable
 {
@@ -97,6 +170,40 @@ sealed class Database : IDisposable
         return Convert.ToInt32(result);
     }
 
+    public List<MovieListItem> GetMovies()
+    {
+        using var command = _connection.CreateCommand();
+        command.CommandText = """
+            SELECT
+                m.title,
+                m.year,
+                m.genre,
+                r.rating,
+                m.imdb_url
+            FROM movies AS m
+            INNER JOIN ratings AS r ON r.id = m.rating_id
+            ORDER BY m.title, m.year;
+        """;
+
+        using var reader = command.ExecuteReader();
+        var movies = new List<MovieListItem>();
+
+        while (reader.Read())
+        {
+            movies.Add(
+                new MovieListItem(
+                    reader.GetString(0),
+                    reader.GetInt32(1),
+                    reader.GetString(2),
+                    reader.GetString(3),
+                    reader.IsDBNull(4) ? null : reader.GetString(4)
+                )
+            );
+        }
+
+        return movies;
+    }
+
     public void Dispose()
     {
         if (_disposed)
@@ -108,3 +215,4 @@ sealed class Database : IDisposable
 }
 
 record Movie(string Title, int Year, string Genre, int RatingId, string? ImdbUrl = null);
+record MovieListItem(string Title, int Year, string Genre, string Rating, string? ImdbUrl = null);
